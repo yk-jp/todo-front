3a164a3a4420e0b8c60d3293eb6368abe0144756
yk-jp <k82.y12@gmail.com>
2022-11-19 15:22:11 -0800

    default home page

---
+++ pages/home/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+      
+      </Head>
+
+      <main>
+        
+      </main>
+
+      <footer>
+      
+      </footer>
+    </div>
+  )
+}
