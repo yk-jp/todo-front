72fe96d854db092b09cdd7a8e5edffd635481275
yk-jp <k82.y12@gmail.com>
2022-11-19 21:16:35 -0800

    create input field

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,9 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faListAlt } from "@fortawesome/free-regular-svg-icons";
+import style from "./style.module.css";
 
 export default function Home() {
   return (
-    <div >
-      <main>
-      </main>
+    <div
+      className={`${style.flex} ${style.alignItemsCenter} ${style.justifyCenter} ${style.mt3}`}
+    >
+      <div
+        id="todo-wrapper"
+        className={`${style.container} ${style.borderRadius}`}
+      >
+        <div
+          id="input-wrapper"
+          className={`${style.inputWrapper}  ${style.borderRadius}`}
+        >
+          <FontAwesomeIcon icon={faListAlt} className={style.inputIcon}/>
+          <input
+            type="text"
+            className={style.inputField}
+            placeholder="Add a Text"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
